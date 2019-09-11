56d49f4c4821c05ef15b38294395c4b0749f8bf5
Kashyap Kale <43263855+kashyapkale@users.noreply.github.com>
2019-09-11 15:30:12 +0530

    Create fade.py

---
+++ IOT/fade.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO   
+import time               
+
+GPIO.setmode(GPIO.BCM) 
+                         
+
+GPIO.setup(18, GPIO.OUT)  
+pwm = GPIO.PWM(18, 100)   
+
+dc=0                             
+pwm.start(dc)                      
+while True:                      
+    for dc in range(0, 101, 5):    
+      pwm.ChangeDutyCycle(dc)
+      time.sleep(0.1)             
+      print(dc)
+    for dc in range(95, 0, -5):    
+      pwm.ChangeDutyCycle(dc)
+      time.sleep(0.1)             
+      print(dc)
+      
+pwm.stop()                         
+GPIO.cleanup()  
